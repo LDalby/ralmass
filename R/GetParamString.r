2c41b538625ce50b728d46b0982b0cd40cf07c01
LDalby <lars@bios.au.dk>
2016-08-11 12:33:41 +0200

    Split also on (

--- R/GetParamString.r
+++ R/GetParamString.r
@@ -7,11 +7,18 @@
 #' string.
 #' @return The character string of the config parameter
 #' @export 
-	GetParamString = function(config = NULL) {
-		if(is.null(config)){
-			stop('Input parameter config missing')
-		}
-	valuestring = stringr::str_split(config[1], '=')
+GetParamString = function(config = NULL) {
+	if(is.null(config)){
+		stop('Input parameter config missing')
+	}
+	if(length(grep('(', config)) > 0) 
+	{
+		valuestring = stringr::str_split(config[1], '(')
+	}
+	if(length(grep('(', config)) == 0) 
+	{
+		valuestring = stringr::str_split(config[1], '=')
+	} 
 	value = stringr::str_trim(valuestring[[1]][1])
 	return(value)
 }

fedd10a2575568484fa35b7a3ca13e4a49d28fe8
LDalby <lars@bios.au.dk>
2016-11-07 12:27:36 +0100

    Cut the x-axis to meaingful range

--- R/PlotEnergetics.r
+++ R/PlotEnergetics.r
@@ -55,6 +55,7 @@ PlotEnergetics = function(SimData, FieldData, Sample = NULL) {
 	geom_smooth(aes(group = Season)) +
 	theme_bw() + 
 	theme(axis.text=element_text(size=8)) + 
-	scale_x_date(date_breaks = "1 month", date_labels = "%b")
+	scale_x_date(date_breaks = "1 month", date_labels = "%b", 
+		limits = c(lubridate::date("2012-10-01"), lubridate::date("2013-02-28"))
 	return(p)
 }

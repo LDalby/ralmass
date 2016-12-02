7a9cf8e7410257fc693bfd8be1c8e94ce768c3d4
LDalby <lars@bios.au.dk>
2016-12-02 15:36:07 +0100

    Add SSSE option to CalcHabUseFit

--- R/CalcHabitatUseFit.r
+++ R/CalcHabitatUseFit.r
@@ -6,9 +6,15 @@
 #'
 #' @param FieldData data.table The table with the field data
 #' @param SimData data.table The field forage output file from ALMaSS
+#' @param measure character The method to use for estimating the fit. Either
+#' 'SSSE' or 'LS'
 #' @return data.table The fit per species and month
 #' @export
-CalcHabitatUseFit = function(FieldData, SimData) {
+CalcHabitatUseFit = function(FieldData, SimData, measure = NULL) {
+	if(is.null(measure)) 
+	{
+		stop('Input parameter missing')
+	}
 # Field data:
 	fielddata = FieldData[, .(Month, HabitatUse, N, Species)]
 	h = unique(fielddata[, HabitatUse])
@@ -20,9 +26,8 @@ CalcHabitatUseFit = function(FieldData, SimData) {
 	fielddata = rbind(fielddata, fieldcombs)
 	cols = c('Species', 'HabitatUse', 'Month')
 	fielddata = fielddata[, lapply(.SD, sum, na.rm = TRUE), by = cols]
-	fielddata[, NMTotal:=sum(N), by=.(Month, Species)]
-	fielddata[, PropField:=N/NMTotal]
-	fielddata[, c('N', 'NMTotal'):=NULL]
+	fielddata[, PropField:=N/sum(N), by=.(Month, Species)]
+	fielddata[, N:=NULL]
 	data.table::setkeyv(fielddata, cols)
 # field data end
 	
@@ -44,9 +49,9 @@ CalcHabitatUseFit = function(FieldData, SimData) {
 # Full data
 	hb = rbind(hbpf, hbbn, hbgl)
 	hb = hb[complete.cases(hb),]
-	hbpfMonths = unique(hb[Species == 'Pinkfoot', Month])
-	hbglMonths = unique(hb[Species == 'Greylag', Month])
-	hbbnMonths = unique(hb[Species == 'Barnacle', Month])
+	hbpfMonths = hb[Species == 'Pinkfoot', unique(Month)]
+	hbglMonths = hb[Species == 'Greylag', unique(Month)]
+	hbbnMonths = hb[Species == 'Barnacle', unique(Month)]
 # Fill in zeros in all habitat classes
 	totalh = unique(c(hb[,HabitatUse], h))
 	combs = expand.grid(N = 0, HabitatUse = totalh, Month = mths,
@@ -54,25 +59,30 @@ CalcHabitatUseFit = function(FieldData, SimData) {
 	combs = data.table::as.data.table(combs)
 
 	temp = rbind(hb, combs)
-	temp[,NMTotal:=sum(N), by=.(Month, Species)]
-	temp[, PropSim:=N/NMTotal]
+	temp[,PropSim:=N/sum(N), by=.(Month, Species)]
 	temp = temp[, lapply(.SD, sum, na.rm = TRUE), by = cols]
 	temp = unique(temp[, .(HabitatUse, Month, Species, PropSim)][is.na(PropSim), PropSim:=0])
 	data.table::setkeyv(temp, cols)
 
 	HabFit = merge(temp, fielddata)
-	HabFit = HabFit[, Fit:=1-sum(abs(PropSim-PropField)), by = cols]
-	
-	# Knock out cases where popn in sim went extinct:
-	HabFit[Species == 'Barnacle' & !Month %in% hbbnMonths, Fit:=NA]
-	HabFit[Species == 'Greylag' & !Month %in% hbglMonths, Fit:=NA]
-	HabFit[Species == 'Pinkfoot' & !Month %in% hbpfMonths, Fit:=NA]
-	# HabFit[, Month:=as.factor(Month)]
-	# Calculate the sesonal fit as the mean of the monthly fits:
-	HabFit[, MonthlyFit:=sum(Fit, na.rm = TRUE)/length(h), by = c('Species', 'Month')]
-	HabFit = unique(HabFit[!is.na(Fit), .(Species, MonthlyFit)])
-	HabFit[,Fit:=sum(MonthlyFit)/.N, by = 'Species']
-	HabFit = unique(HabFit[, .(Species, Fit)])
-	# data.table::setkeyv(HabFit, cols)
-	return(HabFit)
-}
+	if(measure == 'SSSE') {
+		HabFit = HabFit[PropField > 0, SSE:=(PropSim-PropField)^2/PropField, by = cols]
+		# Sum to get the SSSE
+		HabFit = HabFit[, sum(SSE, na.rm = TRUE), by = Species]
+		setnames(HabFit, c('Species', 'SSSE'))
+		return(HabFit)
+		}
+		if(measure == 'LS') {
+			HabFit = HabFit[, Fit:=1-sum(abs(PropSim-PropField)), by = cols]
+			# Knock out cases where popn in sim went extinct:
+			HabFit[Species == 'Barnacle' & !Month %in% hbbnMonths, Fit:=NA]
+			HabFit[Species == 'Greylag' & !Month %in% hbglMonths, Fit:=NA]
+			HabFit[Species == 'Pinkfoot' & !Month %in% hbpfMonths, Fit:=NA]
+			# Calculate the sesonal fit as the mean of the monthly fits:
+			HabFit[, MonthlyFit:=sum(Fit, na.rm = TRUE)/length(h), by = c('Species', 'Month')]
+			HabFit = unique(HabFit[!is.na(Fit), .(Species, MonthlyFit)])
+			HabFit[,Fit:=sum(MonthlyFit)/.N, by = 'Species']
+			HabFit = unique(HabFit[, .(Species, Fit)])
+			return(HabFit)
+		}
+	}

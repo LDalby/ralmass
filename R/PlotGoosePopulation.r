4ea16912ef21607796e6039a44feb7a1a71dcc5a
ldalby <lars.dalby@gmail.com>
2017-08-07 16:32:56 +0200

    Use dplyr/tidyr and rem option to plot dates (now default)

--- R/PlotGoosePopulation.r
+++ R/PlotGoosePopulation.r
@@ -2,26 +2,25 @@
 #'
 #' Plot the number of geese per species and type
 #'
-#' @param data data.table The ALMaSS data file GoosePopulationData.txt
-#' @param dates logical Should we plot day numbers as dates?
+#' @param data tibble The ALMaSS data file GoosePopulationData.txt
 #' @return A nice plot
 #' @export
 PlotGoosePopulation = function(data, dates = FALSE){
-	melted = data.table::melt(data, id.vars = c('Day', 'SeasonNumber'),
-		variable.name = 'GooseType', value.name = 'Numbers')
-	melted = melted[Numbers != 0,]
-	if(dates) {
-	melted[, Day:=as.Date(Day, origin = '2010-01-01')]
-	}
-	p = ggplot2::ggplot(melted, ggplot2::aes(Day, Numbers, group = GooseType)) +
+	# Remodel:
+  molten <- tidyr::gather_(data, gather_cols = c("Day", "SeasonNumber"),
+		key_col = "GooseType", value_col = "Numbers") %>%
+	  dplyr::filter(Numbers > 0) %>%
+    dplyr::mutate(Date = as.Date(Day, origin = '2010-01-01')) %>%
+	# Plot:
+	  ggplot2::ggplot(ggplot2::aes(Date, Numbers, group = GooseType)) +
 		ggplot2::geom_line(ggplot2::aes(colour = GooseType)) +
 		ggplot2::geom_point(ggplot2::aes(colour = GooseType)) +
-		ggplot2::ylab('Numbers') + ggplot2::theme_bw()
-	if(dates) {
-	  p = p + ggplot2::scale_x_date(date_breaks = "1 month", date_labels = "%b") +
-	    ggplot2::xlab('Month')
-	}
-	return(p + scale_color_brewer(palette = 'Paired'))
+		ggplot2::ylab('Numbers') +
+	  ggplot2::theme_bw() +
+	  ggplot2::scale_x_date(date_breaks = "1 month", date_labels = "%b") +
+	  ggplot2::xlab('Month') +
+	  ggplot2::scale_color_brewer(palette = 'Paired')
+	return(p)
 }
 
 

13aa5f5a2c2aa7417ae13baf551cfa1b3f0caaa2
Lars Dalby <lars.dalby@gmail.com>
2017-11-06 13:22:20 +0100

    Don't import plyr and gridExtra

--- R/ralmass-package.r
+++ R/ralmass-package.r
@@ -4,10 +4,9 @@
 #' @docType package
 #' @import stringr
 #' @import RColorBrewer
-#' @import plyr
 #' @import data.table
 #' @import dplyr
-#' @import gridExtra
 #' @import ggplot2
 #' @import tibble
+#' @import tidyr
 NULL

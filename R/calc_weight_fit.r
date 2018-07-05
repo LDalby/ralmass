016b4337e192ba398d1ceeaf2b61ecc321843a8b
LDalby <lars.dalby@gmail.com>
2018-07-05 14:03:49 +0200

    tidy name and adapt to tidy almass headers

--- R/calc_weight_fit.r
+++ R/calc_weight_fit.r
@@ -6,19 +6,19 @@
 #' @param field data.frame The observed weights
 #' @return numeric The calculated fit.
 #' @export
-CalcWeightFit = function(sim = NULL, field = NULL) {
+calc_weight_fit = function(sim = NULL, field = NULL) {
 	if(any(is.null(sim), is.null(field)))
 	{
 		stop('Input parameter missing')
 	}
   # Calculate least squares
-  themin <- min(sim$MeanWeight, field$MeanWeightField)
-  themax <- max(sim$MeanWeight, field$MeanWeightField)
+  themin <- min(sim$mean_weight, field$mean_weight_field)
+  themax <- max(sim$mean_weight, field$mean_weight_field)
   denum <- themax-themin
   inner_join(sim, field, by = "week") %>%
-    mutate(MeanWeight = (MeanWeight-themin)/denum,
-           MeanWeightField = (MeanWeightField-themin)/denum) %>%
-    summarise(fit = 1-sum((MeanWeight-MeanWeightField)^2)/n_distinct(week)) %>%
+    mutate(mean_weight = (mean_weight-themin)/denum,
+           mean_weight_field = (mean_weight_field-themin)/denum) %>%
+    summarise(fit = 1-sum((mean_weight-mean_weight_field)^2)/n_distinct(week)) %>%
     pull(fit) -> fit
 
   assertthat::assert_that(fit > 0,

fe2110e34090621dbe380ac8f07db5d640118b77
LDalby <lars.dalby@gmail.com>
2018-08-21 10:31:44 +0200

    Catch cases where config parameter is missing from the config file.

--- R/GetParamValue.r
+++ R/GetParamValue.r
@@ -1,21 +1,22 @@
 #' Get the value of a config variable
 #'
 #' Return the value of a config variable in a specified ALMaSS config file.
-#' 
+#'
 #' @param config character Path to the file to read start and leave date from
 #' @param param character The name of the config variables
 #' @return The value of the config parameter
-#' @export 
-	GetParamValue = function(param = NULL, config = NULL) {
-		if(is.null(param)){
-			stop('Input parameter param missing')
+#' @export
+	GetParamValue = function(param, config = NULL) {
+		if(missing(param)){
+			stop("Input parameter param missing")
 		}
-		if(!is.null(config)) 
+		if(!is.null(config))
 		{
-			param = config[grep(param, config)]
-			param = param[length(param)]  # Get the last mention of param
+			param <- config[grep(param, config)]
+			param <- param[length(param)]  # Get the last mention of param
 		}
-	valuestring = stringr::str_split(param[1], '=')
-	value = as.numeric(stringr::str_trim(valuestring[[1]][2]))
+	valuestring <- stringr::str_split(param[1], '=')
+	value <- as.numeric(stringr::str_trim(valuestring[[1]][2]))
+	if(is.na(value)) stop("Config variable not found")
 	return(value)
 }

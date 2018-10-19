d8e24658c8af5144e89b7bc20744f8a87c82ae94
LDalby <lars.dalby@gmail.com>
2018-10-19 11:21:42 +0200

    Update object naming and use glue

--- R/edit_bat.r
+++ R/edit_bat.r
@@ -1,47 +1,47 @@
 #' Edit bat file or shell script
 #'
-#' Edit a bat file or shell script to reflect the correct number of runs
+#' Edit a shell script to reflect the correct number of runs
 #' in a scenario. This function requires the ParameterValues.txt file
 #' to be present in work_dir. The function reads the number of runs
 #' from that file.
 #'
 #' @param work_dir character Path to the work directory with the ALMaSS exe file
-#' @param bat_file character File name for the shell file doing the loop
+#' @param sh_file character File name for the shell file doing the loop
 #' @export
-edit_bat = function(work_dir, bat_file) {
+edit_bat = function(work_dir, sh_file) {
 	if(missing(work_dir))
 	{
 		stop("Input parameter work_dir is missing")
 	}
-  if(missing(bat_file))
+  if(missing(sh_file))
   {
-    stop("Input parameter bat_file is missing")
+    stop("Input parameter sh_file is missing")
   }
 	# Get the number of runs:
 	param_file <- fs::path(work_dir, "ParameterValues.txt")
 	if(!fs::file_exists(param_file)) {
 		stop(glue::glue("ParameterValues.txt missing from {work_dir}"))
 	}
-	paramvals <- data.table::fread(param_file)
-	n_params <- length(paramvals[, unique(V1)])
-	runs <- nrow(paramvals)/n_params
+	param_vals <- data.table::fread(param_file)
+	n_params <- length(param_vals[, unique(V1)])
+	runs <- nrow(param_vals)/n_params
 
-	bat_path <- fs::path(work_dir, bat_file)
-	if(!fs::file_exists(bat_path)) {
-	  stop(glue::glue("{bat_path} missing"))
+	sh_file_path <- fs::path(work_dir, sh_file)
+	if(!fs::file_exists(sh_file_path)) {
+	  stop(glue::glue("{sh_file_path} missing"))
 	}
-	bat <- readLines(bat_file)
-	if(length(bat) == 0) {
-		stop("Error reading bat file")
+	sh <- readLines(sh_file_path)
+	if(length(sh) == 0) {
+		stop(glue::glue("Error reading {sh_file_path}"))
 	}
 
- for_line <- glue::glue("for i in {{01..{runs}}")
- index <- grep("for i in ", bat)
- if(length(index) == 0) {
-    stop("Cannot find the for loop in the bat file")
+  for_line <- glue::glue("for i in {{01..{runs}}")
+  index <- grep("for i in ", sh)
+  if(length(index) == 0) {
+    stop(glue::glue("Cannot find the for loop in {sh_file}"))
   }
-	bat[index] <- for_line
-	filecon <- file(bat_path, open = "wt")
+	sh[index] <- for_line
+	filecon <- file(sh_file_path, open = "wt")
 	write(bat, file = filecon)
 	close(filecon)
 }
